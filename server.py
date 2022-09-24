cd28035e4b7d788416daf6c42b4a6ff010803cd9
mcadena05 <cadenamarlynn@gmail.com>
2022-09-24 07:58:20 -0600

    Initial commit

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, flash, session, redirect
+from model import connect_to_db,  User, db
+import crud
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route("/")
+def homepage():
+    # View homepage and About Me
+
+    return render_template("homepage.html")
+
+@app.route("/quote", methods=["POST"])
+def request_quote():
+    # "Create a new user."
+
+    name = request.form.get("name")
+    email = request.form.get("email")
+    phone = request.form.get('phone')
+    message = request.form.get("message")
+    
+
+
+    return redirect("/")
+
+
+
+if __name__ == "__main__":
+    connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
